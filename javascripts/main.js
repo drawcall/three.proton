6882311a88c27069b8af37d2a8bc852989c3dae3
A-JIE <a-jie.cn@msn.com>
2016-10-29 13:09:16 +0800

    pagesite update

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,44 @@
-console.log('This would be the main JS file.');
+$(function() {
+    $(".show-case > div").on("mouseenter", function() {
+        var mask = $(this).find(".mask");
+        mask.addClass("trans");
+        setTimeout(showInMask, 50, mask);
+    });
+
+    $(".show-case .mask").each(function() {
+        showOutMask($(this));
+    });
+
+    $(".show-case > div").on("mouseleave", function() {
+        var mask = $(this).find(".mask");
+        showOutMask(mask);
+    });
+
+    function showInMask(mask) {
+        var ins = "translate(0,0)";
+        mask.css({
+            "opacity": 1,
+            "height": "110%",
+            "top": "0",
+            "bottom": "auto"
+        });
+
+        setTimeout(function(argument) {
+            mask.css({
+                "height": "0",
+                "bottom": "0",
+                "top": "auto"
+            });
+        }, 500);
+    }
+
+    function showOutMask(mask) {
+        var outs = "translate(0,120px)";
+        mask.css({
+            "opacity": 1,
+            "height": "0%",
+            "bottom": "0",
+            "top": "auto"
+        });
+    }
+});
